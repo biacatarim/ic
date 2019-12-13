7d1a738cc3f5b47462e1569a925c46e9f33fd62e
biacatarim <58833452+biacatarim@users.noreply.github.com>
2019-12-12 22:15:56 -0300

    Add files via upload

---
+++ arduino-quase-final.ino
@@ -0,0 +1,129 @@
+/*
+ * Projeto: Expansao de portas analogicas 8 canais do Arduino Nano V3.0 e envio das leituras por serial.
+ * Autor: FilipeFlop e Pedro Bertoleti
+ * Data: Janeiro/2018
+ */
+ 
+//Defines
+#define TAMANHO_STRING_SERIAL 30     //((4*6) + 7 + 1)  //6 canais (de 4 bytes de informação cada, em ASCII) e 7 separadores (;) e 1 terminador de string (\0)
+#define ldrpin 0 
+//Variáveis globais
+int LeiturasADC[6]; //armazenará as leituras dos canais de ADC
+ 
+//protótipops
+void FazLeituraCanaisADC(void);
+void TransmiteLeiturasADC(void);
+
+//Porta ligada ao pino IN1 do modulo
+int porta_rele1 = 14; //lampada
+//Porta ligada ao pino IN2 do modulo
+int porta_rele2 = 15; //ventilador
+//Porta ligada ao botao 1
+int porta_botao1 = 17; 
+//Porta ligada ao botao 2
+int porta_botao2 = 18;
+ 
+//Armazena o estado do rele - 0 (LOW) ou 1 (HIGH)
+int estadorele1 = 1;
+int estadorele2 = 1;
+//Armazena o valor lido dos botoes
+int leitura1 = 0;
+int leitura2 = 0;
+ 
+/*
+ * Implementações
+ */
+ 
+//Função: faz a leitura dos canais de ADC
+//Parâmetros: nenhum
+//Retorno: nenhum
+void FazLeituraCanaisADC(void)
+{
+  char i;
+ 
+  for(i=0; i<8; i++)
+    LeiturasADC[i] = analogRead(i);
+}
+ 
+//Função: Transmite via serial, na forma textual/string, as leituras de ADC obtidas
+//Parâmetros: nenhum
+//Retorno: nenhum
+void TransmiteLeiturasADC(void)
+{
+   char InfoCanaisADC[TAMANHO_STRING_SERIAL];
+ 
+   //limpa string
+   memset(InfoCanaisADC,0,TAMANHO_STRING_SERIAL);
+ 
+   //coloca as leituras numa string
+   sprintf(InfoCanaisADC,"%04d;%04d;%04d;%04d;%04d;%04d;%04d;%04d", LeiturasADC[0], 
+                                                                    LeiturasADC[1], 
+                                                                    LeiturasADC[2], 
+                                                                    LeiturasADC[3], 
+                                                                    LeiturasADC[4], 
+                                                                    LeiturasADC[5]);
+                                                                  
+ 
+   //Transmite a string pela serial
+   Serial.write(InfoCanaisADC, TAMANHO_STRING_SERIAL);                                                                     
+}
+  
+void setup() 
+{
+  memset(LeiturasADC,0,sizeof(LeiturasADC));
+   
+  //configura o baudrate da comunicação serial em 19200
+  Serial.begin(19200); 
+  //Define pinos para o rele como saida
+  pinMode(porta_rele1, OUTPUT); 
+  pinMode(porta_rele2, OUTPUT);
+  //Define pinos dos botoes como entrada
+  pinMode(porta_botao1, INPUT); 
+  pinMode(porta_botao2, INPUT);
+  //Estado inicial dos reles - desligados
+  digitalWrite(porta_rele1, HIGH);
+  digitalWrite(porta_rele2, HIGH);
+    
+}
+ 
+void loop() {
+
+ int readLdr = analogRead(ldrpin);
+ readLdr = LeiturasADC[0];
+delay(500);
+  FazLeituraCanaisADC();  
+  TransmiteLeiturasADC();
+
+    //Verifica o acionamento do botao 1
+  leitura1 = digitalRead(porta_botao1);
+  if (leitura1 != 0)
+  {
+    while(digitalRead(porta_botao1) != 0)
+    {
+      delay(100);
+    }
+    //Inverte o estado da porta
+    estadorele1 = !estadorele1;
+    //Comandos para o rele 1
+    digitalWrite(porta_rele1, estadorele1);  
+  }   
+   
+  //Verifica o acionamento do botao 2
+  leitura2 = digitalRead(porta_botao2);
+  if (leitura2 != 0)
+  {
+    while(digitalRead(porta_botao2) != 0)
+    {
+      delay(100);
+    }
+    //Inverte o estado da porta
+    estadorele2 = !estadorele2;
+    //Comandos para o rele 2
+    digitalWrite(porta_rele2, estadorele2);  
+
+   if (readLdr < 7) {
+     digitalWrite(porta_rele1, LOW);
+   }
+  
+  } 
+}
---
+++ teste-quase-final.ino
@@ -0,0 +1,287 @@
+
+ 
+#include <ESP8266WiFi.h> // Importa a Biblioteca ESP8266WiFi
+#include <PubSubClient.h> // Importa a Biblioteca PubSubClien
+ 
+ 
+#define TAMANHO_STRING_SERIAL 30  //8 canais (de 4 bytes de informação cada, em ASCII) e 7 separadores (;) e terminador de string (\0)
+ 
+#include <ESP8266WiFi.h>
+#include <DHT.h>  // Including library for dht
+
+#define DHTPIN 14          //pin where the dht11 is connected
+ 
+DHT dht(DHTPIN, DHT11);
+
+#define SENSOR_LED                    4
+#define CO_CONTENT_INPUT              A0
+#define HEATER_5V                     60000
+#define HEATER_1_4_V                  90000
+#define READING_DELAY_MILLIS          5000
+#define PIN_BUZZER 0
+const char* g_ssid      = "BIA";
+const char* g_password  = "nodemcu123";
+const char* g_host      = "184.106.153.149";
+const char* g_apiKey   = "R38QN4LKNKG88ISA";
+
+unsigned long g_startMillis;
+unsigned long g_switchTimeMillis;
+boolean       g_heaterInHighPhase;
+
+char StringLeiturasADC[TAMANHO_STRING_SERIAL]; 
+
+//Prototypes
+void initSerial(void);
+void initWiFi(void);
+
+void reconectWiFi(void); 
+
+bool VerificaSeHaInformacaoNaSerial(void);
+
+ 
+/*
+ * Implementações
+ */
+ 
+//Função: inicializa comunicação serial com baudrate 115200 (para comunicação com Arduino Nano)
+//Parâmetros: nenhum
+//Retorno: nenhum
+void initSerial() 
+{
+    Serial.begin(19200);
+}
+ 
+//Função: inicializa e conecta-se na rede WI-FI desejada
+//Parâmetros: nenhum
+//Retorno: nenhum
+void initWiFi() 
+{
+    delay(10);
+    reconectWiFi();
+}
+  
+
+  
+//Função: reconecta-se ao WiFi
+//Parâmetros: nenhum
+//Retorno: nenhum
+void reconectWiFi() 
+{
+    //se já está conectado a rede WI-FI, nada é feito. 
+    //Caso contrário, são efetuadas tentativas de conexão
+    if (WiFi.status() == WL_CONNECTED)
+        return;
+         
+    WiFi.begin(g_ssid, g_password); // Conecta na rede WI-FI
+     
+    while (WiFi.status() != WL_CONNECTED) 
+        delay(100);  
+}
+ 
+ 
+ 
+
+//Função: verifica se há informações sendo recebidas na serial.
+//        Em caso positivo, as recebe e armazena (para futuro envio por MQTT)
+//Parâmetros: nenhum
+//Retorno: true: ha informações recebidas
+//         false: não ha informações recebidas
+bool VerificaSeHaInformacaoNaSerial(void)
+{
+  char c;
+  char i;
+   
+  if (Serial.available() <= 0)
+    return false;
+ 
+  //há dados sendo recebidos. Limpa buffer de recepção:
+  memset(StringLeiturasADC,0,TAMANHO_STRING_SERIAL);  
+ 
+  //pega a string toda (até \0).
+  i=0;
+  do {
+    c = Serial.read();
+    if (c != '\0') 
+    {
+      StringLeiturasADC[i] = c;
+      i++;
+    }
+  } while (c != '\0');
+ 
+  return true;  
+}
+ 
+ 
+void setup() 
+{
+    initSerial();
+    initWiFi();
+Serial.begin(115200);
+  Serial.println();
+  Serial.println();
+  pinMode(PIN_BUZZER, OUTPUT);
+  Serial.print("Connecting to ");
+  Serial.println(g_ssid);
+  
+  WiFi.begin(g_ssid, g_password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");  
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  
+  pinMode(SENSOR_LED, OUTPUT);
+  
+  g_startMillis = millis();
+  
+  switchHeaterHigh();
+  
+  Serial.println("Elapsed Time (s), Gas Level");
+    
+
+}
+ 
+void loop() 
+{
+  if(g_heaterInHighPhase){
+    // 5v phase of cycle. see if need to switch low yet
+    if(millis() > g_switchTimeMillis) {
+      switchHeaterLow();
+    }
+  }
+  else {
+    // 1.4v phase of cycle. see if need to switch high yet
+    if(millis() > g_switchTimeMillis) {
+      switchHeaterHigh();
+    }
+  }
+  
+  presentGasLevel();
+  delay(READING_DELAY_MILLIS);
+
+ Serial.println(StringLeiturasADC[0]);
+  
+  WiFiClient client;
+  const int l_httpPort = 80;
+  if (!client.connect(g_host, l_httpPort)) {
+    Serial.println("Thingspeak Connection Failed");
+    return;
+  }
+  String url = "/update?key=";
+  url += g_apiKey;
+  url +="&field4=";
+  url += String(StringLeiturasADC[0]);
+ 
+  
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+             "Host: " + g_host + "\r\n" + 
+             "Connection: close\r\n\r\n");
+   while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  
+}
+}
+
+/*********************************************************************************
+Function Name         : turnHeaterHigh
+Description           : Trun on the Heater by Providing 5V to the CO Sensor
+Parameters            : void
+Return                : void
+*********************************************************************************/
+void switchHeaterHigh(void){
+  // 5v phase
+  digitalWrite(SENSOR_LED, LOW);
+  g_heaterInHighPhase = true;
+  g_switchTimeMillis = millis() + HEATER_5V;
+}
+
+/*********************************************************************************
+Function Name         : turnHeaterLow
+Description           : Trun off the Heater by Providing 1.4V to the CO Sensor
+Parameters            : void
+Return                : void
+*********************************************************************************/
+void switchHeaterLow(void){
+  // 1.4v phase
+  digitalWrite(SENSOR_LED, HIGH);
+  g_heaterInHighPhase = false;
+  g_switchTimeMillis = millis() + HEATER_1_4_V;
+}
+
+
+/*********************************************************************************
+Function Name         : presentGasLevel
+Description           : read the present CO content in the Air as a analog value
+Parameters            : void
+Return                : void
+*********************************************************************************/
+void presentGasLevel(){
+ 
+   float h = dht.readHumidity();
+      float t = dht.readTemperature();
+      
+              if (isnan(h) || isnan(t)) 
+                 {
+                     Serial.println("Failed to read from DHT sensor!");
+                      return;
+                 }
+  unsigned int l_gasLevel = analogRead(CO_CONTENT_INPUT);
+  unsigned int time = (millis() - g_startMillis) / 1000;
+  Serial.print(l_gasLevel);
+  if (l_gasLevel > 25){//SE VALOR LIDO NO PINO ANALÓGICO FOR MAIOR QUE O VALOR LIMITE, FAZ 
+  digitalWrite(PIN_BUZZER, HIGH);
+  }
+  //LOW : nenhum movimento detectado
+  else{
+    //desativa o buzzer
+    digitalWrite(PIN_BUZZER, LOW);
+  }
+
+  WiFiClient client;
+  const int l_httpPort = 80;
+  if (!client.connect(g_host, l_httpPort)) {
+    Serial.println("Thingspeak Connection Failed");
+    return;
+  }
+
+  String url = "/update?key=";
+  url += g_apiKey;
+  url +="&field1=";
+  url += String(t);
+  url +="&field2=";
+  url += String(h);
+  url += "&field3=";
+  url += String(l_gasLevel);
+  //Serial.println(url);
+  
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+             "Host: " + g_host + "\r\n" + 
+             "Connection: close\r\n\r\n");
+  delay(10);
+  
+    Serial.print("Temperature: ");
+                             Serial.print(t);
+                             Serial.print(" degrees Celcius, Humidity: ");
+                             Serial.print(h);
+                             Serial.println("%. Send to Thingspeak.");
+                        
+   
+  
+ 
+ 
+          Serial.println("Waiting...");
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+  Serial.print("ThingSpeak Connection Closed");
+  delay(10000);
+
+}
+
